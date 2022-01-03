8492968a3e30ea1061bd5d00a085109d508d8190
hassan3111 <ali.hassan@student.moringaschool.com>
2022-01-04 00:22:44 +0300

    add initial review

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookstoreapp.apps.BookstoreappConfig',
+    'bootstrap3',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'bookstoreapp/static'),]
+
