4d9a82cb2ac5db67bfaa794b6af1db8761f50680
JoyWambui <joywambuindegwa@gmail.com>
2022-01-06 23:57:46 +0300

    Create normal user and author

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookstoreapp.apps.BookstoreappConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,13 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'bookstore',
+        'USER': 'bobo',
+        'PASSWORD': 'Riptide',
+        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- bookstoreapp/models.py
+++ bookstoreapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+#create custoclass User(AbstractUser):
+class User(AbstractUser):
+    is_normal_user = models.BooleanField(default=False)
+    is_author = models.BooleanField(default=False)
+
+class NormalUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
