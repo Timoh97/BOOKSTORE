cf38f8171a957c3d68664e0972e79b10882d3c66
Victoria045 <victoriaberyl45@gmail.com>
2022-01-10 12:42:31 +0300

    create the index and register views

--- application/views.py
+++ application/views.py
@@ -1,29 +1,53 @@
-from django.shortcuts import render, redirect
-from .forms import BookForm
-from .models import Books
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404,HttpResponseRedirect, JsonResponse
+from django.http import JsonResponse
+from .forms import BookForm, UserRegisterForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+import json
+import datetime
+from .models import *
+from .utils import cookieCart, cartData, guestOrder
+from .forms import UserRegisterForm,ProductForm
 
 # Create your views here.
+def index(request):
+	data = cartData(request)
 
+	cartItems = data['cartItems']
+	order = data['order']
+	items = data['items']
 
-def upload(request):
-	if request.method == "POST":
-		form = BookForm(request.POST, request.FILES)
-		if form.is_valid():
-			form.save()
-		return redirect("/")
-	form = BookForm()
-	books = Books.objects.all()
-	return render(request=request, template_name="upload.html", context={'form':form, 'books':books})
+	products = Product.objects.all()
+	if request.method == 'POST':
+			form = ProductForm(request.POST, request.FILES)
+			# import pdb; pdb.set_trace()
+			if form.is_valid():
+					post = form.save(commit=False)
+					post.user = request.user
+					post.save()
+					return HttpResponseRedirect(request.path_info)
+	else:
+			form = ProductForm()
+	params = {
+        'products':products,
+				'form': form, 
+        'cartItems':cartItems
+        }
+	return render(request, 'order/index.html', params)
 
-def books(request):
-    if request.method == "POST":
-        form = BookForm(request.POST, request.FILES)
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
-            project = form.save(commit=False)
-            project.save()
-        return redirect('/')
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            messages.success(request, f'Your account has been created! You are now able to log in {username}.')
+            return redirect('login')
     else:
-        form = BookForm()
-        
-    return render(request, 'books.html', {"form": form})
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form': form}) 
+
