5b9bf1e2f4364c92a92d881b6b4b0108158cdbde
Victoria045 <victoriaberyl45@gmail.com>
2022-01-10 16:58:16 +0300

    modify customer model

--- application/models.py
+++ application/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 import cloudinary
 from cloudinary.models import CloudinaryField
 
@@ -12,13 +14,14 @@ class Customer(models.Model):
     def __str__(self):
       return self.name
 
+    def save_customer(self):
+        self.user
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True, blank=True)
     image = CloudinaryField('image')
-    author = models.CharField(default='Author name..',max_length=100)
-    year_published = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
       return self.name
@@ -82,3 +85,9 @@ class ShippingAddress(models.Model):
 
 	def __str__(self):
 		return self.address    
+
+# @receiver(post_save, sender=User)
+# def create_user_customer(sender, instance, created, **kwargs):
+#   if created:
+#     print('created')
+#     Customer.objects.create(user=instance)
