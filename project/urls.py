db4ddb797939a80ff6946e55a3164434db80c957
Timoh97 <timothy.nthiga@student.moringaschool.com>
2022-01-11 16:29:55 +0300

    files

--- project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings #add this
-from django.conf.urls.static import static #add this
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include ('application.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- virtual/bin/django-admin.py
+++ virtual/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/moringa/BOOKSTORE/virtual/bin/python
+#!/home/moringa/Desktop/BOOKSTORE/virtual/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
