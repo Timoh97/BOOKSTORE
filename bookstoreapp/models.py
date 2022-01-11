98c52d84b7715e37ff5f0cec90a3daa7153767a7
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-11 10:23:24 +0300

    CReated the user profile model

--- bookstoreapp/models.py
+++ bookstoreapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# The profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    bio = models.TextField(max_length=400, blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    profile_pic = models.ImageField(upload_to='images/')
