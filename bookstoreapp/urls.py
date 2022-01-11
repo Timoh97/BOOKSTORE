5390cede54ef4de1c74d03cdfe0ca4f6aa7bbaab
Annaliskirwa <annaliskirwa@gmail.com>
2022-01-11 11:11:24 +0300

    Added the profile urls

---
+++ bookstoreapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls.conf import path
+from . import views
+
+urlpatterns=[
+    path('',views.index,name='home'),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('user_profile/<username>/', views.user_profile, name='user_profile'),
+]
