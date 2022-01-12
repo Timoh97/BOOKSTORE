695c76f1c7a064f4dc7b0af9f9db66c050b249dc
Victoria045 <victoriaberyl45@gmail.com>
2022-01-12 17:01:43 +0300

    move file from ft/orders to ft/order

---
+++ bookstoreapp/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Customer,Product
+
+# Create your forms here.
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('image','name', 'price', 'digital',)
---
+++ bookstoreapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+	  path('cart/', views.cart, name="cart"),
+	  path('checkout/', views.checkout, name="checkout"),
+	  path('update_item/', views.updateItem, name="update_item"),
+	  path('process_order/', views.processOrder, name="process_order"),
+    path('login/',auth_views.LoginView.as_view(), name='login'),
+    path('register/',views.register, name='register'),
+    path('account/', include('django.contrib.auth.urls')),
+   
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
